812f0a68b6177118e56df506a1486828de7b94df
bpiyush <piyushnbagad11@gmail.com>
2025-10-15 12:29:51 +0100

    Minor

--- scripts/train_debug.sh
+++ scripts/train_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT_DIR="/work/piyush/experiments/CaRe/debug_run_nli45k+ego4d-5k"
+OUTPUT_DIR="/work/piyush/experiments/CaRe/debug_run_nli-9k+ego4d-1k"
 RUN_NAME=`basename $OUTPUT_DIR`
 
 args=()
@@ -8,8 +8,8 @@ args=()
 BASE_MODEL="/work/piyush/pretrained_checkpoints/CaRe-7B-Stage-1"
 # BATCH_SIZE=768
 # MICRO_BATCH_SIZE=32
-BATCH_SIZE=16
-MICRO_BATCH_SIZE=2
+BATCH_SIZE=32
+MICRO_BATCH_SIZE=4
 EPOCH=2
 LR=2e-5
 WARMUP_RATIO=0.1
@@ -20,7 +20,8 @@ NUM_NODES=1
 # CSV_PATH='/scratch/shared/beegfs/piyush/datasets/SimCSE-NLI/nli_for_simcse.csv'
 # CSV_PATH='/scratch/shared/beegfs/piyush/datasets/SimCSE-NLI/nli_for_simcse-10k.csv'
 # CSV_PATH='/scratch/shared/beegfs/piyush/datasets/SimCSE-NLI/nli+ego4d-20k.csv'
-CSV_PATH='/scratch/shared/beegfs/piyush/datasets/SimCSE-NLI/nli45k+ego4d-5k.csv'
+# CSV_PATH='/scratch/shared/beegfs/piyush/datasets/SimCSE-NLI/nli45k+ego4d-5k.csv'
+CSV_PATH='/scratch/shared/beegfs/piyush/datasets/SimCSE-NLI/nli-9k+ego4d-1k.csv'
 
 echo $BASE_MODEL
 echo $MICRO_BATCH_SIZE $BATCH_SIZE
