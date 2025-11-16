401d9dc03155fb93d37ba2a4be2ca2eba9ec5906
bpiyush <piyushnbagad11@gmail.com>
2025-11-16 01:39:31 +0000

    Minor

---
+++ scripts/run_experiments_tarsier.sh
@@ -0,0 +1,4 @@
+bash scripts/train_tarsier.sh final-10112025/nli_4500+ego_500-seed_42
+bash scripts/train_tarsier.sh final-10112025/nli_18000+ego_2000-seed_42
+bash scripts/train_tarsier.sh final-10112025/nli_45000+ego_5000-seed_42
+bash scripts/train_tarsier.sh final-10112025/nli_90000+ego_10000-seed_42
