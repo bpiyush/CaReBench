84d7c956ce19adecb661e261617c145ef24918cc
bpiyush <piyushnbagad11@gmail.com>
2025-11-18 17:33:25 +0000

    Minir

--- tasks/egocvr_compute_metrics.py
+++ tasks/egocvr_compute_metrics.py
@@ -61,15 +61,25 @@ if __name__ == "__main__":
         f"{save_dir}/tarsier+tara_query_embeddings_egocvr-frames_15.pt",
     )
     
-    df_gall = df_gall[
-        df_gall['query_id'].isin(set(query_embeddings_video_only.keys()))
-    ].reset_index(drop=True)
+    query_embeddings = torch.load(f"{save_dir}/tarsier+tara_query_embeddings_egocvr-frames_15.pt")
+    df_gall['query_id'] = df_gall[['video_clip_id', 'instruction']].apply(
+        lambda x: f"{x[0]}|{x[1]}", axis=1,
+    )
+    df_gall = df_gall[df_gall['query_id'].isin(set(query_embeddings))].reset_index(drop=True)
+    print("Number of rows in df_gall: ", len(df_gall))
+    
+    # df_gall = df_gall[
+    #     df_gall['query_id'].isin(set(query_embeddings_video_only.keys()))
+    # ].reset_index(drop=True)
+    # df_gall = df_gall[df_gall.video_clip_id.isin(set(query_embeddings_video_only.keys()))].reset_index(drop=True)
 
 
     df_gall_reranked = []
     n_reranked = 15
-    for query_id in su.log.tqdm_iterator(df_gall.query_id.unique(), desc='Re-ranking'):
-        row_query = df_gall[df_gall.query_id == query_id].iloc[0].to_dict()
+    for i in su.log.tqdm_iterator(range(len(df_gall)), desc='Re-ranking'):
+        # row_query = df_gall[df_gall.video_clip_id == query_id].iloc[0].to_dict()
+        row_query = df_gall.iloc[i].to_dict()
+        query_id = row_query['video_clip_id']
         zq = query_embeddings_video_only[query_id]
 
         # Local candidates
@@ -85,14 +95,57 @@ if __name__ == "__main__":
         sim_local = zq @ zl.T
         sim_global = zq @ zg.T
         
-        reranked_indices_local = sim_local.topk(k=n_reranked).indices
-        reranked_indices_global = sim_global.topk(k=n_reranked).indices
-        local_idx_reranked = str(list(reranked_indices_local.cpu().numpy()))
-        global_idx_reranked = str(list(reranked_indices_global.cpu().numpy()))
+        reranked_indices_local = sim_local.topk(k=min(n_reranked, len(local_idx))).indices
+        reranked_indices_global = sim_global.topk(k=min(n_reranked, len(global_idx))).indices
+        reranked_indices_global = np.array(global_idx)[reranked_indices_global]
+        reranked_indices_local = np.array(local_idx)[reranked_indices_local]
+        local_idx_reranked = str(list(reranked_indices_local))
+        global_idx_reranked = str(list(reranked_indices_global))
 
         row_query_new = deepcopy(row_query)
         row_query_new['local_idx'] = local_idx_reranked
         row_query_new['global_idx'] = global_idx_reranked
         df_gall_reranked.append(row_query_new)
     df_gall_reranked = pd.DataFrame(df_gall_reranked)
-    import ipdb; ipdb.set_trace()
+    
+    metrics_local = {1: [], 2: [], 3: []}
+    metrics_global = {1: [], 5: [], 10: []}
+    for query_id in su.log.tqdm_iterator(query_embeddings, desc='Iterating over queries'):
+        zq = query_embeddings[query_id]
+        row_query = df_gall_reranked[df_gall_reranked.query_id == query_id].iloc[0].to_dict()
+
+        # Local candidates
+        local_idx = eval(row_query['local_idx'])
+        rows_local = df_base.iloc[local_idx]
+        zl = torch.stack([local_gallery_embeddings[x] for x in rows_local.clip_name])
+
+        # Global candidates
+        global_idx = eval(row_query['global_idx'])
+        rows_global = df_base.iloc[global_idx]
+        zg = torch.stack([global_gallery_embeddings[x] for x in rows_global.clip_name])
+
+        target_clip_ids = eval(row_query['target_clip_ids'])
+        target_indices_local = np.where(rows_local.clip_name.isin(target_clip_ids))[0]
+        target_indices_global = np.where(rows_global.clip_name.isin(target_clip_ids))[0]
+
+        sim_local = zq @ zl.T
+        for k in metrics_local:
+            metrics_local[k].append(
+                set(target_indices_local).intersection(set((sim_local.topk(k=min(k, len(sim_local)))).indices.tolist()))
+            )
+
+        sim_global = zq @ zg.T
+        for k in metrics_global:
+            metrics_global[k].append(
+                set(target_indices_global).intersection(set((sim_global.topk(k=min(k, len(sim_global)))).indices.tolist()))
+            )
+
+    print("Local metrics:")
+    for k, v in metrics_local.items():
+        acc = np.round(np.mean([len(y) > 0 for y in v]) * 100., 2)
+        print(k, acc)
+
+    print("Global metrics:")
+    for k, v in metrics_global.items():
+        acc = np.round(np.mean([len(y) > 0 for y in v]) * 100., 2)
+        print(k, acc)
