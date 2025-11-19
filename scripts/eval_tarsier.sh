d3f654b9437b2d2c5ed10484b58a2b14baf5a8e1
bpiyush <piyushnbagad11@gmail.com>
2025-11-19 13:30:34 +0000

    Minor

--- scripts/eval_tarsier.sh
+++ scripts/eval_tarsier.sh
@@ -13,32 +13,32 @@ base_model_name=$(basename $BASE_MODEL)
 OUTPUT_DIR="/work/piyush/experiments/CaRe/${base_model_name}/${split}"
 echo "Using output directory: $OUTPUT_DIR"
 
-# Merge weights
-echo "Merging weights..."
-python tasks/merge_weights.py \
-    -b $BASE_MODEL \
-    -f $OUTPUT_DIR
+# # Merge weights
+# echo "Merging weights..."
+# python tasks/merge_weights.py \
+#     -b $BASE_MODEL \
+#     -f $OUTPUT_DIR
 
-# Evaluate on SSv2
-echo "[::::::] Evaluating SSv2 [::::::]"
+# # Evaluate on SSv2
+# echo "[::::::] Evaluating SSv2 [::::::]"
+# python notebooks/eval_care_retrieval.py \
+#     --model_id $OUTPUT_DIR/merged_checkpoint \
+#     --device_map $device_map \
+#     --dataset ssv2
+# echo "[:::::::::::::::::::::::::::::::]"
+
+# Evaluate on epic
+echo "[::::::] Evaluating EPIC [::::::]"
 python notebooks/eval_care_retrieval.py \
     --model_id $OUTPUT_DIR/merged_checkpoint \
     --device_map $device_map \
-    --dataset ssv2
+    --dataset epic
 echo "[:::::::::::::::::::::::::::::::]"
 
-# # Evaluate on epic
-# echo "[::::::] Evaluating EPIC [::::::]"
-# python notebooks/eval_care_retrieval.py \
-#     --model_id $OUTPUT_DIR/merged_checkpoint \
-#     --device_map $device_map \
-#     --dataset epic
-# echo "[:::::::::::::::::::::::::::::::]"
-
-# # Evaluate on charades
-# echo "[::::::] Evaluating Charades [::::::]"
-# python notebooks/eval_care_retrieval.py \
-#     --model_id $OUTPUT_DIR/merged_checkpoint \
-#     --device_map $device_map \
-#     --dataset charades
-# echo "[:::::::::::::::::::::::::::::::]"
+# Evaluate on charades
+echo "[::::::] Evaluating Charades [::::::]"
+python notebooks/eval_care_retrieval.py \
+    --model_id $OUTPUT_DIR/merged_checkpoint \
+    --device_map $device_map \
+    --dataset charades
+echo "[:::::::::::::::::::::::::::::::]"
